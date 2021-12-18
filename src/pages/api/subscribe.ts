7732c449822515d7cd79b7b852e3fb9f2d626893
Felipe Silva <feehlipeeh422@gmail.com>
2021-12-18 12:46:13 -0300

    :test_tube: feat

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -9,7 +9,7 @@ type User = {
     id: string;
   };
   data: {
-    stripe_costumer_id: string;
+    stripe_customer_id: string;
   };
 };
 
@@ -22,26 +22,26 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
       q.Get(q.Match(q.Index('user_by_email'), q.Casefold(session.user.email)))
     );
 
-    let costumerId = user.data.stripe_costumer_id;
+    let customerId = user.data.stripe_customer_id;
 
-    if (!costumerId) {
-      const stripeCostumer = await stripe.customers.create({
+    if (!customerId) {
+      const stripecustomer = await stripe.customers.create({
         email: session.user.email,
       });
 
       await fauna.query(
         q.Update(q.Ref(q.Collection('users'), user.ref.id), {
           data: {
-            stripe_costumer_id: stripeCostumer.id,
+            stripe_customer_id: stripecustomer.id,
           },
         })
       );
 
-      costumerId = stripeCostumer.id;
+      customerId = stripecustomer.id;
     }
 
     const stripeCheckoutSession = await stripe.checkout.sessions.create({
-      customer: costumerId,
+      customer: customerId,
       payment_method_types: ['card'],
       billing_address_collection: 'required',
       line_items: [{ price: 'price_1K7JycBEj3Nqxkmq0ZQTrdV7', quantity: 1 }],
